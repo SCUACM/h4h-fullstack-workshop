92b0f35f6aa48e219bab39c502cfb122ae67519c
Jordan Mosakowski <35510468+jordanmosakowski@users.noreply.github.com>
2023-02-18 15:41:47 -0800

    complete

--- backend/server.py
+++ backend/server.py
@@ -1,13 +1,23 @@
 import requests
+from flask_cors import CORS
 
-response = requests.get('https://raw.githubusercontent.com/SCUACM/h4h-fullstack-workshop/main/data.json')
+from flask import Flask, jsonify
 
-print(response.json())
+app = Flask(__name__)
+CORS(app)
 
-dictionary = {
-  "year": 2023, 
-  "name": "Hack for Humanity 2023!", 
-  "data": [1, 2, 3, 4, 5]
-}
+@app.route("/")
+def get_api_call():
+    response = requests.get('https://raw.githubusercontent.com/SCUACM/h4h-fullstack-workshop/main/data.json')
 
-print(dictionary)
+    return response.json()
+
+@app.route("/dict")
+def get_dictionary():
+    dictionary = {
+        "year": 2023, 
+        "name": "Hack for Humanity 2023!", 
+        "data": [1, 2, 3, 4, 5]
+    }
+
+    return jsonify(dictionary)
--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,31 @@
 console.log("Hello world");
+
+function getApi1() {
+    fetch("http://127.0.0.1:5000/dict").then((response) => response.json()).then((data) => {
+        console.log(data);
+        if(data.name) {
+            document.getElementById("title").innerHTML = data.name;
+        }
+        if(data.data) {
+            document.getElementById("content1").innerHTML = data.data;
+        }
+    })
+}
+
+function getApi2() {
+    fetch("http://127.0.0.1:5000/").then((response) => response.json()).then((data) => {
+        console.log(data);
+        let count = data.count;
+        console.log(count);
+        let newContent = "";
+        for(let i=0; i<count; i++) {
+            newContent+= `
+                <div>
+                    <h3>Name: ${data.members[i].name} (${data.members[i].gradYear})</h3>
+                    ${data.members[i].role}
+                </div>
+            `;
+        }
+        document.getElementById("content2").innerHTML = newContent;
+    })
+}
